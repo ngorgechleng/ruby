7bb4d91e51f1380460027e0a4cf0af0afe41e85e
ngorgechleng <ngorgechleng@yahoo.com>
2012-04-21 22:46:47 -0700

    example table of contents

---
+++ table_of_content.rb
@@ -0,0 +1,5 @@
+puts ' Table of Contents'.center(60)
+puts ''
+puts 'Capter 1 = Getting Started'.ljust(30) + 'page  1'.rjust(30)
+puts 'Capter 2 = Numbers'.ljust(30) + 'page  9'.rjust(30)
+puts 'Capter 3 = Letters'.ljust(30) + 'page 13'.rjust(30)
