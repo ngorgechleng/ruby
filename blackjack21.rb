46bdef70aabdca62185c5ce31c725a9de83b89cc
ngorgechleng <ngorgechleng@yahoo.com>
2012-04-21 22:37:30 -0700

    clean up

--- blackjack21.rb
+++ blackjack21.rb
@@ -1,13 +1,10 @@
 def blackjack21(*numbers)
-	sum = 0
-	numbers.each do |number|
-		sum=sum+number
-	end
-	if sum==21
-		return true
-	else
-		return false
-	end
+  sum = 0
+  numbers.each do |number|
+    sum = sum + number
+  end
+
+  sum == 21
 end
 
 puts blackjack21(1, 2, 3)
